2440d689c831700c7b9066f5d430f96b083c8181
Macdonald Williams <mw9078612@gmail.com>
2025-08-09 03:33:45 +0000

    On the Medical GP dashboard, can you place these 3 place cards under the

--- src/app/gp/page.tsx
+++ src/app/gp/page.tsx
@@ -298,10 +298,12 @@ export default function GpDashboardPage() {
         </div>
         <div className="space-y-6">
             <NosyCorner />
-            <PatientCommunication />
-            <ReportingAnalytics />
-             <SystemComplianceCard />
         </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
+          <PatientCommunication />
+          <ReportingAnalytics />
+          <SystemComplianceCard />
       </div>
     </div>
   );
